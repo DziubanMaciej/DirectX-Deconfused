84af8cb940e1c7918da34557c88270913f09b3b2
MaciejDziuban <dziuban.maciej@gmail.com>
2019-07-22 21:03:01 +0200

    Refactor VertexBuffer and IndexBuffer

--- LibraryDX12/Api/MeshImpl.cpp
+++ LibraryDX12/Api/MeshImpl.cpp
@@ -73,7 +73,7 @@ void MeshImpl::uploadToGPU() {
     const UINT verticesCount = static_cast<UINT>(vertices.size() / 3);
     const UINT vertexSize = 3 * sizeof(FLOAT);
     this->vertexBuffer = std::make_unique<VertexBuffer>(device, commandList, vertices.data(), verticesCount, vertexSize);
-    this->indexBuffer = std::make_unique<IndexBuffer>(device, commandList, indices.data(), static_cast<UINT>(indices.size()), static_cast<UINT>(sizeof(UINT)));
+    this->indexBuffer = std::make_unique<IndexBuffer>(device, commandList, indices.data(), static_cast<UINT>(indices.size()));
     commandList.close();
 
     // Execute and register obtained allocator and lists to the manager
--- LibraryDX12/Source/VertexOrIndexBuffer.h
+++ LibraryDX12/Source/VertexOrIndexBuffer.h
@@ -6,43 +6,40 @@
 #include "DXD/ExternalHeadersWrappers/d3d12.h"
 
 // ---------------------------------------------------------------------------------------------------------------- Template class handling index or vertex buffer
-template <typename _ViewType>
+
 class VertexOrIndexBuffer : public Resource {
 public:
-    VertexOrIndexBuffer(ID3D12DevicePtr device, CommandList &commandList, const void *data, UINT numElements, UINT elementSize)
-        : Resource(device, D3D12_HEAP_TYPE_DEFAULT, D3D12_HEAP_FLAG_NONE, numElements * elementSize, D3D12_RESOURCE_STATE_COPY_DEST, nullptr) {
-        recordGpuUploadCommands(device, commandList, data, getDestinationResourceState());
-        populateView(numElements, elementSize);
+    VertexOrIndexBuffer(ID3D12DevicePtr device, CommandList &commandList, const void *data, UINT size, D3D12_RESOURCE_STATES resourceStateToTransition)
+        : Resource(device, D3D12_HEAP_TYPE_DEFAULT, D3D12_HEAP_FLAG_NONE, size, D3D12_RESOURCE_STATE_COPY_DEST, nullptr) {
+        recordGpuUploadCommands(device, commandList, data, resourceStateToTransition);
     }
-
-    auto getView() const { return view; }
-private:
-    void populateView(UINT numElements, UINT elementSize);
-    static D3D12_RESOURCE_STATES getDestinationResourceState();
-
-    _ViewType view = {};
 };
 
-// ---------------------------------------------------------------------------------------------------------------- Instantation for vertex buffer
-template <>
-inline void VertexOrIndexBuffer<D3D12_VERTEX_BUFFER_VIEW>::populateView(UINT numElements, UINT elementSize) {
-    view.BufferLocation = resource->GetGPUVirtualAddress();
-    view.SizeInBytes = elementSize * numElements;
-    view.StrideInBytes = elementSize;
-}
-inline D3D12_RESOURCE_STATES VertexOrIndexBuffer<D3D12_VERTEX_BUFFER_VIEW>::getDestinationResourceState() {
-    return D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER;
-}
-using VertexBuffer = VertexOrIndexBuffer<D3D12_VERTEX_BUFFER_VIEW>;
+class VertexBuffer : public VertexOrIndexBuffer {
+public:
+    VertexBuffer(ID3D12DevicePtr device, CommandList &commandList, const void *data, UINT verticesCount, UINT vertexSize)
+        : VertexOrIndexBuffer(device, commandList, data, verticesCount * vertexSize, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER) {
+        view.BufferLocation = resource->GetGPUVirtualAddress();
+        view.SizeInBytes = verticesCount * vertexSize;
+        view.StrideInBytes = vertexSize;
+    }
+    auto getView() const { return view; }
 
-// ---------------------------------------------------------------------------------------------------------------- Instantation for index buffer
-template <>
-inline void VertexOrIndexBuffer<D3D12_INDEX_BUFFER_VIEW>::populateView(UINT numElements, UINT elementSize) {
-    view.BufferLocation = resource->GetGPUVirtualAddress();
-    view.SizeInBytes = elementSize * numElements;
-    view.Format = DXGI_FORMAT_R32_UINT;;
-}
-inline D3D12_RESOURCE_STATES VertexOrIndexBuffer<D3D12_INDEX_BUFFER_VIEW>::getDestinationResourceState() {
-    return D3D12_RESOURCE_STATE_INDEX_BUFFER;
-}
-using IndexBuffer = VertexOrIndexBuffer<D3D12_INDEX_BUFFER_VIEW>;
+private:
+    D3D12_VERTEX_BUFFER_VIEW view = {};
+};
+
+class IndexBuffer : public VertexOrIndexBuffer {
+public:
+    // TODO Format is hardcoded. This should take format as an argument and deduce index size base on its
+    IndexBuffer(ID3D12DevicePtr device, CommandList &commandList, const void *data, UINT indicesCount)
+        : VertexOrIndexBuffer(device, commandList, data, indicesCount * sizeof(UINT), D3D12_RESOURCE_STATE_INDEX_BUFFER) {
+        view.BufferLocation = resource->GetGPUVirtualAddress();
+        view.SizeInBytes = indicesCount * sizeof(UINT);
+        view.Format = DXGI_FORMAT_R32_UINT;
+    }
+    auto getView() const { return view; }
+
+private:
+    D3D12_INDEX_BUFFER_VIEW view = {};
+};
