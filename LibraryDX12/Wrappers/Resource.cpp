6dafab5eb80ec9d79369340cca5660489e6f5da1
MaciejDziuban <dziuban.maciej@gmail.com>
2019-07-22 13:28:07 +0200

    Fix some warnings

--- LibraryDX12/Wrappers/Resource.cpp
+++ LibraryDX12/Wrappers/Resource.cpp
@@ -11,7 +11,7 @@ Resource::Resource(ID3D12DevicePtr device, const D3D12_HEAP_PROPERTIES *pHeapPro
     create(device, pHeapProperties, heapFlags, pDesc, initialResourceState, pOptimizedClearValue);
 }
 
-Resource::Resource(ID3D12DevicePtr device, D3D12_HEAP_TYPE heapType, D3D12_HEAP_FLAGS heapFlags, const int bufferSize, D3D12_RESOURCE_STATES initialResourceState, const D3D12_CLEAR_VALUE *pOptimizedClearValue) {
+Resource::Resource(ID3D12DevicePtr device, D3D12_HEAP_TYPE heapType, D3D12_HEAP_FLAGS heapFlags, UINT64 bufferSize, D3D12_RESOURCE_STATES initialResourceState, const D3D12_CLEAR_VALUE *pOptimizedClearValue) {
     create(device, &CD3DX12_HEAP_PROPERTIES(heapType), heapFlags, &CD3DX12_RESOURCE_DESC::Buffer(bufferSize), initialResourceState, pOptimizedClearValue);
 }
 
--- LibraryDX12/Wrappers/Resource.h
+++ LibraryDX12/Wrappers/Resource.h
@@ -12,7 +12,7 @@ class Resource : DXD::NonCopyable {
 public:
     explicit Resource(ID3D12ResourcePtr resource);
     explicit Resource(ID3D12DevicePtr device, const D3D12_HEAP_PROPERTIES *pHeapProperties, D3D12_HEAP_FLAGS heapFlags, const D3D12_RESOURCE_DESC *pDesc, D3D12_RESOURCE_STATES initialResourceState, const D3D12_CLEAR_VALUE *pOptimizedClearValue);
-    explicit Resource(ID3D12DevicePtr device, D3D12_HEAP_TYPE heapType, D3D12_HEAP_FLAGS heapFlags, const int bufferSize, D3D12_RESOURCE_STATES initialResourceState, const D3D12_CLEAR_VALUE *pOptimizedClearValue);
+    explicit Resource(ID3D12DevicePtr device, D3D12_HEAP_TYPE heapType, D3D12_HEAP_FLAGS heapFlags, UINT64 bufferSize, D3D12_RESOURCE_STATES initialResourceState, const D3D12_CLEAR_VALUE *pOptimizedClearValue);
     Resource(Resource &&other) = default;
     Resource &operator=(Resource &&other) = default;
     auto &getResource() { return resource; };
