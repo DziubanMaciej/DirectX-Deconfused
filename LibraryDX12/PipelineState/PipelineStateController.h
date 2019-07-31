ffa18b0dca7e257a11c11e2fcda2de942fe7735d
MaciejDziuban <dziuban.maciej@gmail.com>
2019-07-31 17:31:41 +0200

    Move RootSignature to separate file

--- LibraryDX12/PipelineState/PipelineStateController.h
+++ LibraryDX12/PipelineState/PipelineStateController.h
@@ -4,14 +4,10 @@
 
 #include "DXD/ExternalHeadersWrappers/d3dcompiler.h"
 #include "DXD/ExternalHeadersWrappers/d3dx12.h"
-#include <cassert>
-#include <map>
 #include <string>
-#include <vector>
 
 class PipelineStateController : DXD::NonCopyableAndMovable {
 public:
-    class RootSignature;
     enum class Identifier {
         PIPELINE_STATE_DEFAULT = 0,
         PIPELINE_STATE_TEXTURE = 1,
@@ -41,40 +37,3 @@ private:
     constexpr static char pixelShaderTarget[] = "ps_5_1";
     constexpr static char vertexShaderTarget[] = "vs_5_1";
 };
-
-class PipelineStateController::RootSignature : DXD::NonCopyableAndMovable {
-public:
-    template <typename ConstantType>
-    RootSignature &append32bitConstant(D3D12_SHADER_VISIBILITY visibility);
-    RootSignature &appendShaderResourceView(D3D12_ROOT_DESCRIPTOR_FLAGS flags, D3D12_SHADER_VISIBILITY visibility);
-    RootSignature &appendConstantBufferView(D3D12_ROOT_DESCRIPTOR_FLAGS flags, D3D12_SHADER_VISIBILITY visibility);
-    RootSignature &appendUnorderedAccessView(D3D12_ROOT_DESCRIPTOR_FLAGS flags, D3D12_SHADER_VISIBILITY visibility);
-    RootSignature &appendStaticSampler(const D3D12_STATIC_SAMPLER_DESC &samplerDescription);
-    RootSignature &appendDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE rangeType, UINT numDescriptors, D3D12_SHADER_VISIBILITY visibility);
-
-    ID3D12RootSignaturePtr compile(ID3D12DevicePtr device);
-
-private:
-    UINT getNextShaderRegisterAndIncrement(D3D12_DESCRIPTOR_RANGE_TYPE rangeType, UINT numDescriptors);
-    void resolveDescriptorRanges();
-    static D3D_ROOT_SIGNATURE_VERSION getHighestRootSignatureVersion(ID3D12DevicePtr device);
-
-    std::map<D3D12_SHADER_VISIBILITY, std::vector<D3D12_DESCRIPTOR_RANGE1>> descriptorRanges; // intermediate data, has to be resolved to descriptor tables
-
-    std::vector<CD3DX12_ROOT_PARAMETER1> rootParameters = {};        // goes directly to D3D12_VERSIONED_ROOT_SIGNATURE_DESC
-    std::vector<D3D12_STATIC_SAMPLER_DESC> samplerDescriptions = {}; // goes directly to D3D12_VERSIONED_ROOT_SIGNATURE_DESC
-
-    UINT nextShaderRegisterT = 0; // SRV
-    UINT nextShaderRegisterU = 0; // UAV
-    UINT nextShaderRegisterB = 0; // CBV, including 32bit constants
-    UINT nextShaderRegisterS = 0; // Sampler
-};
-
-template <typename ConstantType>
-inline PipelineStateController::RootSignature &PipelineStateController::RootSignature::append32bitConstant(D3D12_SHADER_VISIBILITY visibility) {
-    static_assert(sizeof(ConstantType) % 4 == 0, "Not a dword aligned type");
-    constexpr static auto dwordCount = sizeof(ConstantType) / 4;
-    rootParameters.emplace_back();
-    rootParameters.back().InitAsConstants(dwordCount, nextShaderRegisterB++, 0, visibility);
-    return *this;
-}
---
+++ LibraryDX12/PipelineState/RootSignature.cpp
@@ -0,0 +1,103 @@
+#include "RootSignature.h"
+
+#include "Utility/ThrowIfFailed.h"
+
+#include <cassert>
+
+void RootSignature::resolveDescriptorRanges() {
+    for (auto it = descriptorRanges.begin(); it != descriptorRanges.end(); it++) {
+        const D3D12_SHADER_VISIBILITY visibility = it->first;
+        const std::vector<D3D12_DESCRIPTOR_RANGE1> &rangesForGivenVisibility = it->second;
+
+        assert(descriptorRanges.size() > 0);
+
+        rootParameters.emplace_back();
+        rootParameters.back().InitAsDescriptorTable(static_cast<UINT>(rangesForGivenVisibility.size()), rangesForGivenVisibility.data(), visibility);
+    }
+}
+
+RootSignature &RootSignature::appendShaderResourceView(D3D12_ROOT_DESCRIPTOR_FLAGS flags, D3D12_SHADER_VISIBILITY visibility) {
+    rootParameters.emplace_back();
+    rootParameters.back().InitAsShaderResourceView(nextShaderRegisterT++, 0, flags, visibility);
+    return *this;
+}
+
+RootSignature &RootSignature::appendConstantBufferView(D3D12_ROOT_DESCRIPTOR_FLAGS flags, D3D12_SHADER_VISIBILITY visibility) {
+    rootParameters.emplace_back();
+    rootParameters.back().InitAsConstantBufferView(nextShaderRegisterB++, 0, flags, visibility);
+    return *this;
+}
+
+RootSignature &RootSignature::appendUnorderedAccessView(D3D12_ROOT_DESCRIPTOR_FLAGS flags, D3D12_SHADER_VISIBILITY visibility) {
+    rootParameters.emplace_back();
+    rootParameters.back().InitAsUnorderedAccessView(nextShaderRegisterU++, 0, flags, visibility);
+    return *this;
+}
+
+RootSignature &RootSignature::appendStaticSampler(const D3D12_STATIC_SAMPLER_DESC &samplerDescription) {
+    samplerDescriptions.push_back(samplerDescription);
+    return *this;
+}
+
+UINT RootSignature::getNextShaderRegisterAndIncrement(D3D12_DESCRIPTOR_RANGE_TYPE rangeType, UINT numDescriptors) {
+    UINT result{};
+    switch (rangeType) {
+    case D3D12_DESCRIPTOR_RANGE_TYPE_SRV:
+        result = nextShaderRegisterT;
+        nextShaderRegisterT += numDescriptors;
+        break;
+    case D3D12_DESCRIPTOR_RANGE_TYPE_UAV:
+        result = nextShaderRegisterU;
+        nextShaderRegisterU += numDescriptors;
+        break;
+    case D3D12_DESCRIPTOR_RANGE_TYPE_CBV:
+        result = nextShaderRegisterB;
+        nextShaderRegisterB += numDescriptors;
+        break;
+    case D3D12_DESCRIPTOR_RANGE_TYPE_SAMPLER:
+        result = nextShaderRegisterS;
+        nextShaderRegisterS += numDescriptors;
+        unreachableCode(); // TODO Sampler descriptors cannot be in one table with SRVs,UAVs and CBVs. resolveDescriptorRanges() divide them
+    default:
+        unreachableCode();
+    }
+    return result;
+}
+
+RootSignature &RootSignature::appendDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE rangeType, UINT numDescriptors, D3D12_SHADER_VISIBILITY visibility) {
+    const auto baseShaderRegister = getNextShaderRegisterAndIncrement(rangeType, numDescriptors);
+    D3D12_DESCRIPTOR_RANGE1 range{rangeType, numDescriptors, baseShaderRegister, 0, D3D12_DESCRIPTOR_RANGE_FLAG_NONE, D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND};
+    this->descriptorRanges[visibility].push_back(std::move(range));
+    return *this;
+}
+
+D3D_ROOT_SIGNATURE_VERSION RootSignature::getHighestRootSignatureVersion(ID3D12DevicePtr device) {
+    D3D12_FEATURE_DATA_ROOT_SIGNATURE featureData = {};
+    featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
+    if (device->CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, &featureData, sizeof(featureData))) {
+        featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
+    }
+    return featureData.HighestVersion;
+}
+
+ID3D12RootSignaturePtr RootSignature::compile(ID3D12DevicePtr device) {
+    resolveDescriptorRanges();
+    const D3D_ROOT_SIGNATURE_VERSION highestVersion = getHighestRootSignatureVersion(device);
+    const D3D12_ROOT_SIGNATURE_FLAGS rootSignatureFlags =
+        D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |
+        D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS |
+        D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS |
+        D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS;
+    const CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDescription = {
+        static_cast<UINT>(rootParameters.size()), rootParameters.data(),
+        static_cast<UINT>(samplerDescriptions.size()), samplerDescriptions.data(),
+        rootSignatureFlags};
+
+    ID3DBlobPtr rootSignatureBlob = {};
+    ID3DBlobPtr errorBlob = {};
+    throwIfFailed(D3DX12SerializeVersionedRootSignature(&rootSignatureDescription, highestVersion, &rootSignatureBlob, &errorBlob), errorBlob.Get());
+
+    ID3D12RootSignaturePtr rootSignature = {};
+    throwIfFailed(device->CreateRootSignature(0, rootSignatureBlob->GetBufferPointer(), rootSignatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature)));
+    return rootSignature;
+}
