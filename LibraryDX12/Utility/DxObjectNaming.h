09fd601cb39183f4039a65e8a9ac404364e41b91
MaciejDziuban <dziuban.maciej@gmail.com>
2019-09-10 21:20:50 +0200

    Add file that should've been added in 4bc4ae0d693b6a477e48c2b1382c7f5ffb3a8165

---
+++ LibraryDX12/Utility/DxObjectNaming.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Resource/Resource.h"
+
+#include "DXD/ExternalHeadersWrappers/d3d12.h"
+#include <string>
+
+#if defined(_DEBUG)
+template <typename T>
+inline void setObjectName(const Microsoft::WRL::ComPtr<T> &object, const wchar_t *name) {
+    object->SetName(name);
+}
+
+template <typename T, typename... Args>
+inline void setObjectName(const Microsoft::WRL::ComPtr<T> &object, const wchar_t *format, Args &&... args) {
+    wchar_t buffer[1000];
+    swprintf_s(buffer, format, std::forward<Args>(args)...);
+    object->SetName(buffer);
+}
+
+template <typename... Args>
+inline void setObjectName(const Resource &resource, Args &&... args) {
+    setObjectName(resource.getResource(), std::forward<Args>(args)...);
+}
+
+#define SET_OBJECT_NAME(...) setObjectName(__VA_ARGS__);
+
+#else
+#define SET_OBJECT_NAME(...)
+#endif
