623e8209681bc394b678e8b10db31c5bd46f5078
MaciejDziuban <dziuban.maciej@gmail.com>
2019-10-10 20:42:56 +0200

    Add simplistic, hello world app

---
+++ Application/Application2.cpp
@@ -0,0 +1,52 @@
+#include "DXD/DXD.h"
+
+struct Handler : DXD::CallbackHandler {
+    DXD::Object *object = {};
+    void onUpdate(unsigned int deltaTimeMicroseconds) {
+        static float rotation{};
+        rotation += 0.000001f * deltaTimeMicroseconds;
+        object->setRotation(XMFLOAT3{ 0,1,0 }, rotation);
+    }
+};
+
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hprev, LPSTR cmdline, int show) {
+    auto application = DXD::Application::create(true);
+
+    auto mesh = DXD::Mesh::createFromObj(*application, L"Resources/meshes/cube_normals.obj", false, false, true);
+
+    auto object = DXD::Object::create(*mesh);
+    object->setPosition(0, 0, 0);
+    object->setColor(0.2, 0.2, 0.2);
+
+    auto light = DXD::Light::create();
+    light->setType(DXD::LightType::SPOT_LIGHT);
+    light->setDirection(0,0, 1);
+    light->setPower(0.2);
+    light->setPosition(0, 0, -5);
+    light->setColor(0.6f, 0.7f, 0.f);
+
+    auto camera = DXD::Camera::create();
+    camera->setUpDirection(0, 1, 0);
+    camera->setEyePosition(0, 0, -5);
+    camera->setFocusPoint(0, 0, 0);
+    camera->setNearZ(0.001f);
+    camera->setFarZ(1000.0f);
+    camera->setFovAngleYDeg(80);
+
+    auto handler = Handler{};
+    handler.object = object.get();
+
+    auto scene = DXD::Scene::create(*application);
+    scene->setBackgroundColor(0, 0.2f, 0.2f);
+    scene->addObject(*object);
+    scene->addLight(*light);
+    scene->setCamera(*camera);
+
+    auto window = DXD::Window::create(*application, L"myWindow", L"myWindow", hInstance, 800, 600);
+    window->setScene(*scene);
+    application->setCallbackHandler(&handler);
+    window->show();
+    window->messageLoop();
+
+    return 0;
+}
