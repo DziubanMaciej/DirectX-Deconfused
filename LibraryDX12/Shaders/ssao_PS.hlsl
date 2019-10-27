30d39c7c37a20b9a5549ba4677ea39f11bcab382
domin92 <intunek2@interia.pl>
2019-10-27 11:35:12 +0100

    Tick - improve SSAO

--- LibraryDX12/Shaders/ssao_PS.hlsl
+++ LibraryDX12/Shaders/ssao_PS.hlsl
@@ -27,9 +27,28 @@ float doAmbientOcclusion(float2 tcoord, float2 uv, float3 pos, float3 norm) {
     float depth = gBufferDepth.Sample(g_sampler, tcoord + uv).r;
     float3 diff = getPositionFromDepth(tcoord.x + uv.x, tcoord.y + uv.y, depth).xyz - pos;
     const float3 v = normalize(diff);
-    const float d = length(diff) * 2.0f;
-    return max(0.0, dot(norm, v) - 0.2f) * (1.0f / (1.0f + d)) * 3.0f;
+    const float d = length(diff);
+    return max(0.0, dot(norm, v) - 0.2f) * (1.0f / (1.0f + d)) * 2.0f;
 }
+
+static float2 poissonDisk[16] =
+    {
+        float2(0.2770745f, 0.6951455f),
+        float2(0.1874257f, -0.02561589f),
+        float2(-0.3381929f, 0.8713168f),
+        float2(0.5867746f, 0.1087471f),
+        float2(-0.3078699f, 0.188545f),
+        float2(0.7993396f, 0.4595091f),
+        float2(-0.09242552f, 0.5260149f),
+        float2(0.3657553f, -0.5329605f),
+        float2(-0.3829718f, -0.2476171f),
+        float2(-0.01085108f, -0.6966301f),
+        float2(0.8404155f, -0.3543923f),
+        float2(-0.5186161f, -0.7624033f),
+        float2(-0.8135794f, 0.2328489f),
+        float2(-0.784665f, -0.2434929f),
+        float2(0.9920505f, 0.0855163f),
+        float2(-0.687256f, 0.6711345f)};
 
 float main(PixelShaderInput IN) : SV_Target {
 
@@ -46,17 +65,14 @@ float main(PixelShaderInput IN) : SV_Target {
     float3 INnormal = gBufferNormal.Sample(g_sampler, float2(uBase, vBase)).xyz;
 
     float ao = 0.0f;
-    float rad = 2.0f / INdepth;
 
-    for (int i = -2; i < 3; i++) {
-        for (int j = -2; j < 3; j++) {
-            ao = ao + doAmbientOcclusion(float2(uBase, vBase), float2((i * rad) / scb.screenWidth, float(j * rad) / scb.screenHeight), INposition, normalize(INnormal));
-        }
+    for (int i = 0; i < 16; i++) {
+        float2 pos = (poissonDisk[i] / float2(scb.screenWidth, scb.screenHeight)) * 8.0f;
+        ao = ao + doAmbientOcclusion(float2(uBase, vBase), pos, INposition, normalize(INnormal));
     }
 
-    ao = abs(ao / 25.0f);
-    ao = pow(ao, 1.2f);
-    ao = 1 - ao;
+    ao = abs(ao / 16.0f);
+    ao = 1.0f - pow(ao, 1.1f);
 
     return ao;
 }
