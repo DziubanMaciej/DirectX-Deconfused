24d782c7022ea78c73db451a7f2a206f93b730b8
MaciejDziuban <dziuban.maciej@gmail.com>
2019-11-21 02:27:01 +0100

    Add API documentation 1/n

--- LibraryDX12/Include/DXD/Application.h
+++ LibraryDX12/Include/DXD/Application.h
@@ -11,12 +11,28 @@ namespace DXD {
 class CallbackHandler;
 class Settings;
 
+/// \brief Encapsulates global state of the graphics application
+///
+/// Basic class for internal engine usage containing data, constants, information and subsystems common
+/// to the entire engine. There should be precisely one instance of the Application class during correct
+/// application execution. It should be created before any other DXD object and destroyed after all DXD
+/// objects have been freed as well
 class EXPORT Application : NonCopyableAndMovable {
 public:
+    /// Set active CallbackHandler instance which will be handling all internal engine events.
+    /// \param callbackHandler object to set as the active instance, can be null
     virtual void setCallbackHandler(CallbackHandler *callbackHandler) = 0;
+
+    /// Acquire instance to application's settings object
+    /// \return Settings class instance
     virtual Settings &getSettings() = 0;
 
     virtual ~Application() = default;
+
+    /// Factory function used to create Application instance. This function should be called only once during 
+    /// whole execution and should be the first DXD function called.
+    /// \param debugLayer enable diagnostic DirectX debug layer, should be set to false during normal development
+    /// \return Application instance
     static std::unique_ptr<Application> create(bool debugLayer);
 
 protected:
--- LibraryDX12/Include/DXD/CallbackHandler.h
+++ LibraryDX12/Include/DXD/CallbackHandler.h
@@ -6,16 +6,49 @@ namespace DXD {
 
 class Window;
 
+/// \brief Class handling all internal engine events
+///
+/// Application can override any availble event and implement custom handling procedure.
+/// Adding event handling does not alter general engine workflow, e.g. overriding onResize
+/// will not break resizing behaviour of DXD, but rather allow injecting some code to
+/// execute when resize event is triggered.
 class EXPORT CallbackHandler {
 public:
+    /// Called when the active window is resized. Also called upon minimalization with
+    /// both parameters set to 0
+    /// \param newWidth width of the window
+    /// \param newHeight height of the window
     virtual void onResize(int newWidth, int newHeight) {}
-    virtual void onWindowShow(Window &window) {}
+
+    /// Called when user presses keyboard key
+    /// \param vkCode code of pressed key. See https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes for reference
     virtual void onKeyDown(unsigned int vkCode) {}
+
+    /// Called when user releases keyboard key
+    /// \param vkCode code of released key. See https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes for reference
     virtual void onKeyUp(unsigned int vkCode) {}
+
+    /// Called each frame before actual rendering is performed
+    /// \param deltaTimeMicroseconds time elapsed since last frame
     virtual void onUpdate(unsigned int deltaTimeMicroseconds) {}
+
+    /// Called when user scrolls mouse
+    /// \param zDelta difference in wheel position, positive for scrolling up, negative for scrolling down
     virtual void onMouseWheel(int zDelta) {}
+
+    /// Called when user presses left mouse button
+    /// \param xPos screen x coordinate of the mouse during event
+    /// \param yPos screen y coordinate of the mouse during event
     virtual void onLButtonUp(unsigned int xPos, unsigned int yPos) {}
+
+    /// Called when user releases left mouse button
+    /// \param xPos screen x coordinate of the mouse during event
+    /// \param yPos screen y coordinate of the mouse during event
     virtual void onLButtonDown(unsigned int xPos, unsigned int yPos) {}
+
+    /// Called when user moves mouse
+    /// \param xPos new screen x coordinate of the mouse
+    /// \param yPos new screen y coordinate of the mouse
     virtual void onMouseMove(unsigned int xPos, unsigned int yPos) {}
 };
 
