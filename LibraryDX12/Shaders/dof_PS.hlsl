28a7562dfa36c2bfaf94f491da38c7cd1f138429
domin92 <intunek2@interia.pl>
2019-11-24 22:28:41 +0100

    Very primitive dof

--- LibraryDX12/Shaders/dof_PS.hlsl
+++ LibraryDX12/Shaders/dof_PS.hlsl
@@ -14,7 +14,6 @@ struct DofCB {
 
 ConstantBuffer<DofCB> scb : register(b1);
 
-
 float4 main(PixelShaderInput IN) : SV_Target {
 
     const float uBase = IN.Position.x / scb.screenWidth;
@@ -22,7 +21,23 @@ float4 main(PixelShaderInput IN) : SV_Target {
 
     float INdepth = gBufferDepth.Sample(g_sampler, float2(uBase, vBase)).r;
 
-    float3 INlightingOutput = lightingOutput.Sample(g_sampler, float2(uBase, vBase)).rgb;
+    float centerDepth = gBufferDepth.Sample(g_sampler, float2(0.5f, 0.5f)).r;
 
-    return float4(INdepth, 0, 0, 1);
+    float depthDiff = smoothstep(0.0f, 0.1f, abs(INdepth - centerDepth));
+
+    int sampleKernelSize = max(1, int(10.0f * depthDiff));
+
+    float3 outputColor = float3(0.0f, 0.0f, 0.0f);
+
+    [loop] for (int i = -sampleKernelSize; i <= sampleKernelSize; i++) {
+        [loop] for (int j = -sampleKernelSize; j <= sampleKernelSize; j++) {
+            float invResWidth = 1.0f / scb.screenWidth;
+            float invResHeight = 1.0f / scb.screenHeight;
+            outputColor += lightingOutput.Sample(g_sampler, float2(uBase + float(i) * invResWidth, vBase + float(j) * invResHeight)).rgb;
+        }
+    }
+
+    outputColor /= float(pow((2.0f * sampleKernelSize + 1.0f), 2.0f));
+
+    return float4(outputColor, 1);
 }
