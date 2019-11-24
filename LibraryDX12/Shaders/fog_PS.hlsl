c397a52f55aefff50a695e1896eae4a197ae4f3d
domin92 <intunek2@interia.pl>
2019-11-24 21:31:25 +0100

    Clean fog shader

--- LibraryDX12/Shaders/fog_PS.hlsl
+++ LibraryDX12/Shaders/fog_PS.hlsl
@@ -19,18 +19,6 @@ struct FogCB {
 
 ConstantBuffer<FogCB> scb : register(b1);
 
-float4 getPositionFromDepth(float u, float v, float depth) {
-    float4 H = float4((u)*2 - 1, (1 - v) * 2 - 1, depth, 1.0);
-    float4 D = mul(scb.projMatrixInverse, H);
-    float4 INworldPosition = mul(scb.viewMatrixInverse, (D / D.w));
-    return INworldPosition;
-}
-
-float4 getNonLinearDepth(float u, float v, float depth) {
-    float4 H = float4(0, 0, depth, 1.0);
-    float4 D = mul(scb.projMatrixInverse, H);
-    return D;
-}
 
 float4 main(PixelShaderInput IN) : SV_Target {
 
@@ -39,14 +27,15 @@ float4 main(PixelShaderInput IN) : SV_Target {
 
     float INdepth = gBufferDepth.Sample(g_sampler, float2(uBase, vBase)).r;
 
-    float3 INlightingOutput = lightingOutput.Sample(g_sampler, float2(uBase, vBase)).rgb;
-
-    float4 position = getPositionFromDepth(uBase, vBase, INdepth);
-    
-    float fogPower = smoothstep(1.0f - scb.fogPower, 1.0f, INdepth);
-
     float3 fogColor = scb.fogColor.xyz;
+
+    if (INdepth >= 1.0f) {
+        return float4(fogColor, 1.0f);
+    }
+
+    float3 INlightingOutput = lightingOutput.Sample(g_sampler, float2(uBase, vBase)).rgb;
     
+    float fogPower = smoothstep(1.0f - clamp(scb.fogPower, 0.0f, 1.0f), 1.0f, INdepth);
 
     float3 output = INlightingOutput * (1.0f - fogPower) + fogColor * fogPower;
 
