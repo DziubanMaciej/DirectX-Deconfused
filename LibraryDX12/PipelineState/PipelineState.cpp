d30fb8fd7d5c74bd39b910ac1a7883d1df9043ca
MaciejDziuban <dziuban.maciej@gmail.com>
2019-11-30 12:26:53 +0100

    Organize methods

--- LibraryDX12/PipelineState/PipelineState.cpp
+++ LibraryDX12/PipelineState/PipelineState.cpp
@@ -1,7 +1,26 @@
+#include "PipelineState.h"
+
 #include "Application/ApplicationImpl.h"
-#include "PipelineState/PipelineState.h"
 #include "PipelineState/RootSignature.h"
 #include "Utility/ThrowIfFailed.h"
+
+// ------------------------------------------------------------- Generic class for pipeline state operations
+
+D3D12_SHADER_BYTECODE PipelineState::loadAndCompileShader(const std::wstring &name, const std::string &target, const D3D_SHADER_MACRO *defines) {
+    ID3DBlob *compiledShader = nullptr;
+    ID3DBlob *errorBlob = nullptr;
+
+    const auto path = std::wstring{SHADERS_PATH} + name;
+    const auto includeHandler = D3D_COMPILE_STANDARD_FILE_INCLUDE;
+    const auto compileFlags = ApplicationImpl::getInstance().areDebugShadersEnabled() ? D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION : UINT(0);
+    const auto result = D3DCompileFromFile(path.c_str(), defines, includeHandler, "main", target.c_str(), compileFlags, 0, &compiledShader, &errorBlob);
+    throwIfFailed(result, errorBlob);
+
+    this->shaderBlobs.emplace_back(compiledShader);
+    return D3D12_SHADER_BYTECODE{compiledShader->GetBufferPointer(), compiledShader->GetBufferSize()};
+}
+
+// ------------------------------------------------------------- Graphics PSO
 
 GraphicsPipelineState &GraphicsPipelineState::VS(const std::wstring &path) {
     description.VS = loadAndCompileShader(path, "vs_5_1", nullptr);
@@ -59,20 +78,7 @@ void GraphicsPipelineState::compile(ID3D12DevicePtr device, ID3D12PipelineStateP
     throwIfFailed(device->CreateGraphicsPipelineState(&description, IID_PPV_ARGS(&pipelineState)));
 }
 
-D3D12_SHADER_BYTECODE PipelineState::loadAndCompileShader(const std::wstring &name, const std::string &target, const D3D_SHADER_MACRO *defines) {
-    ID3DBlob *compiledShader = nullptr;
-    ID3DBlob *errorBlob = nullptr;
-
-    const auto path = std::wstring{SHADERS_PATH} + name;
-    const auto includeHandler = D3D_COMPILE_STANDARD_FILE_INCLUDE;
-
-    const UINT compileFlags = ApplicationImpl::getInstance().areDebugShadersEnabled() ? D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION : 0u;
-    const auto result = D3DCompileFromFile(path.c_str(), defines, includeHandler, "main", target.c_str(), compileFlags, 0, &compiledShader, &errorBlob);
-    throwIfFailed(result, errorBlob);
-
-    this->shaderBlobs.emplace_back(compiledShader);
-    return D3D12_SHADER_BYTECODE{compiledShader->GetBufferPointer(), compiledShader->GetBufferSize()};
-}
+// ------------------------------------------------------------- Compute PSO
 
 ComputePipelineState::ComputePipelineState(RootSignature &rootSignature) {
     description.pRootSignature = rootSignature.getRootSignature().Get();
